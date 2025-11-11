270812bf26629c645fd76c6a2daf0185127cc95e
valentine231 <ugwuvalentine519@gmail.com>
2025-11-11 05:12:42 +0100

    minor adjustment

--- src/Auth/Signin.jsx
+++ src/Auth/Signin.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import useAuth from '../Store/Authstore'
 import Spinner from '../Components/Authloader'
 import Login from './Login'
+import { Eye, EyeOff } from 'lucide-react';
 
 const Signin = () => {
   const { signup, loading, notification } = useAuth()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [isLogin, setIsLogin] = useState(false)
+  const [showPassword, setShowPassword] = useState(false);
+
+
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
   const handleSignup = (e) => {
     e.preventDefault()
@@ -45,7 +50,8 @@ const Signin = () => {
             <h2 className="text-gray-600 font-bold text-center text-3xl mb-4">
               Create Account
             </h2>
-            <form onSubmit={handleSignup} className="space-y-5">
+            <form onSubmit={handleSignup} className="space-y-5"> 
+              <div>
               <input
                 type="email"
                 placeholder="Email"
@@ -54,16 +60,29 @@ const Signin = () => {
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
-
-              <input
-                type="password"
+              </div>
+             
+             <div className='relative'>
+             <input
+                type={showPassword ? "text" : password}
                 placeholder="Password"
                 className="py-3 px-5 w-full border border-gray-400 rounded-lg focus:ring-2 focus:ring-indigo-400 focus:outline-none"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
+               
+               
+               <button
+          type="button"
+          onClick={togglePasswordVisibility}
+          className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+        >
+          {showPassword ? <EyeOff className="w-5 h-5" /> : <Eye className="w-5 h-5" />}
+        </button>
+             </div>
 
+              
               <button
                 type="submit"
                 className="py-3 w-full bg-indigo-600 rounded-lg text-white font-semibold transition duration-300 hover:bg-indigo-700 disabled:opacity-50"
