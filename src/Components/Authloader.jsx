5271d94faaa30fe166d6cc7e8bb1c5d5e23c1eab
valentine231 <ugwuvalentine519@gmail.com>
2025-12-24 22:42:59 +0100

    mergeconflictResolved

--- src/Components/Authloader.jsx
+++
@@ -1,9 +0,0 @@
-import { ClipLoader } from "react-spinners";
-
-const Spinner = ({ size = 35, color = "#4f46e5" }) => (
-  <div className="flex justify-center items-center">
-    <ClipLoader size={size} color={color} />
-  </div>
-);
-
-export default Spinner;
